73be13ed9f2bdfac93017e2315d09dc62abaa67d
Eric Chee <bboyairwreck@gmail.com>
2015-04-26 15:19:29 -0700

    Sync'd homepage with MySQL. Styled call and create buttons.

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,9 +1,80 @@
+var patientID = 17;
+
 $(document).ready(function() {
-    myFunction();
+    fetchData();
 });
 
+function fetchData() {
+    var url = "http://ericchee.com/neverforgotten/getEvents_Patient.php";
 
-function myFunction() {
+    $.ajax(url, {
+        dataType : "json",
+        data : {
+            'n' : patientID
+        },
+        success : ajaxSuccess,
+        error : ajaxError
+    });
+}
+
+
+/*
+
+ <div class="reminderDayWrap">
+     <h2>TODAY</h2>
+     <div class="reminderContent">
+        <div class="reminderLine">
+            <div class="reminderTitle">Dinner with Sarah</div>
+            <div class="reminderTime">9 PM</div>
+        </div>
+        <div class="reminderLine">
+            <div class="reminderTitle">Take medication</div>
+            <div class="reminderTime">10 AM</div>
+        </div>
+     </div>
+ </div>
+ */
+
+function ajaxSuccess(data) {
+    for (var i = 0; i < data.length; i++){
+        var $newCard = $("#reminderTemplate .reminderDayWrap").clone();
+
+        var evTitle = data[i]["EventTitle"];
+        var evDatetime = data[i]["EventTime"];
+        var evDatetimeArr = datetimeFormat(evDatetime);
+        var evDate = evDatetimeArr["date"];
+
+
+        var evTime = evDatetimeArr["time"];
+        var evTimeFormatted = timeFormat(evTime);
+
+        var dateFormatArr = dateFormat(evDate);
+
+        // inject any thing inside of $newTask;
+        $eventTitle = $newCard.find(".reminderTitle");
+        $eventTitle.text(evTitle);
+
+        $eventTime = $newCard.find(".reminderTime");
+        $eventTime.text(evTimeFormatted);
+
+        $eventDate = $newCard.find("h2");
+        $eventDate.text(dateFormatArr["dateLine"]);
+
+        //  dateFormatArr["dayName"] + dateFormatArr["monthName"]
+
+
+        $("#remindersWrap").prepend($newCard);
+    }
 
 }
 
+function ajaxError( xhr, status, errorThrown ) {
+    alert("Sorry, there was Ajax problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+}
+
+$("#callButton").click(function() {
+    fetchData();
+});
