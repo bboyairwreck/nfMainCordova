b273c7d236b5b43094d22051bf5a71213c099749
dwendle <dub_kung@hotmail.com>
2015-05-17 05:58:10 -0700

    created time picker and customized name input

--- www/js/createReminderTime.js
+++ www/js/createReminderTime.js
@@ -1,6 +1,8 @@
 !(function(){
     var eventName = "";
     var eventDate = "";
+    var hours = null;
+    var minutes = null;
 
     $(document).ready(function() {
         myParams = getParams();
@@ -16,15 +18,47 @@
         $("#details").html("<p>" + eventName + "</p><p>" + eventDateString + "</p>");
     });
 
+    $("#amButton").click(function() {
+        $("#pmButton").attr('src', 'img/pm_yellow.png');
+        $(this).attr('src', 'img/am_yellow_selected.png');
+        if ($("#hourNum").val() != "") {
+            hours = parseInt($("#hourNum").val());
+            if (hours == 12) {
+                hours = 0;
+            }
+        }
+        if ($("#minuteNum").val() != "") {
+            minutes = $("#minuteNum").val();
+        }
+    });
+
+    $("#pmButton").click(function() {
+        $("#amButton").attr('src', 'img/am_yellow.png');
+        $(this).attr('src', 'img/pm_yellow_selected.png');
+        if ($("#hourNum").val() != "") {
+            hours = parseInt($("#hourNum").val());
+            if (hours != 12) {
+                hours = hours + 12;
+            }
+        }
+        if ($("#minuteNum").val() != "") {
+            minutes = $("#minuteNum").val();
+        }
+    });
+
     $("#nextTime").click(function(){
-
-        // grab the name
-        var evTime = $("#inputEventTime").val().trim();
-
         // ensure Event Name has something
-        if (evTime.length > 0) {
+        if (hours != null && minutes != null) {
 
-            // Event name in param
+            // grab event time
+            var evTime = "";
+            if (hours < 10) {
+                evTime = "0" + hours + ":" + minutes;
+            } else {
+                evTime = hours + ":" + minutes;
+            }
+
+            // Event time in param
             var eventInfo = [];
             eventInfo["eventName"] = eventName;
             eventInfo["eventDate"] = eventDate;
