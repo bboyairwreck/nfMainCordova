50ae87eb3ad44d380dfa2b5c4ab31ecde062eafd
Eric Chee <bboyairwreck@gmail.com>
2015-05-22 02:58:57 -0700

    Added 'Good morning...' sound to welcome page

--- platforms/ios/www/js/welcome.js
+++ platforms/ios/www/js/welcome.js
@@ -5,3 +5,51 @@ $(document).ready(function() {
     alert(data[0]["GreetingSound"]);
     alert(patID);
 });
+
+document.addEventListener("deviceready", loadTTS, false);
+
+function loadTTS() {
+    //alert("loadTTS running");
+    if (ttsPlugin != null) {
+
+        ttsPlugin.setRate(0.1); // Set voice speed : default is "0.2"
+
+        ttsPlugin.setLanguage("en-us"); // Set voice language : default is "en-US"
+
+        ttsPlugin.initTTS(successCallBack, failCallBack); // Init Plugin : failCallBack doesn't work yet
+
+
+    } else {
+        alert("nope");
+    }
+}
+
+function successCallBack() {
+//                alert("tts worked?");
+    var todaysDate = dateFormat(getCurDateString())["dateLine"];
+    alert(todaysDate);
+
+    ttsPlugin.speak("Good morning Sarah. Today is " + todaysDate); // Say Hello
+}
+function failCallBack() {
+    alert("tts did not work?");
+}
+
+
+function getCurDateString() {
+    var nowDate = new Date();
+    var monthString = "" + (nowDate.getMonth() + 1);
+    var dayString = "" + nowDate.getDate();
+
+    if ((nowDate.getMonth() + 1) < 10) {
+        monthString = "0" + "" + monthString;
+    }
+
+    if (nowDate.getDate() < 10) {
+        dayString = "0" + "" + dayString;
+    }
+
+    var nowDateString = nowDate.getFullYear() + "-" + monthString + "-" + dayString;
+
+    return nowDateString;
+}
--- www/js/welcome.js
+++ www/js/welcome.js
@@ -5,3 +5,44 @@ $(document).ready(function() {
     alert(data[0]["GreetingSound"]);
     alert(patID);
 });
+
+document.addEventListener("deviceready", loadTTS, false);
+
+function loadTTS() {
+    //alert("loadTTS running");
+    if (ttsPlugin != null) {
+        ttsPlugin.setRate(0.1); // Set voice speed : default is "0.2"
+        ttsPlugin.setLanguage("en-us"); // Set voice language : default is "en-US"
+        ttsPlugin.initTTS(successCallBack, failCallBack); // Init Plugin : failCallBack doesn't work yet
+    } else {
+        alert("Error: text to speech plugin is undefined");
+    }
+}
+
+function successCallBack() {
+    var todaysDate = dateFormat(getCurDateString())["dateLine"];
+
+    var name = "Sarah";
+
+    ttsPlugin.speak("Good morning " + name + ". Today is " + todaysDate); // Good Morning Sarah. Today is May 22, 2015.
+}
+function failCallBack() {}
+
+
+function getCurDateString() {
+    var nowDate = new Date();
+    var monthString = "" + (nowDate.getMonth() + 1);
+    var dayString = "" + nowDate.getDate();
+
+    if ((nowDate.getMonth() + 1) < 10) {
+        monthString = "0" + "" + monthString;
+    }
+
+    if (nowDate.getDate() < 10) {
+        dayString = "0" + "" + dayString;
+    }
+
+    var nowDateString = nowDate.getFullYear() + "-" + monthString + "-" + dayString;
+
+    return nowDateString;
+}
