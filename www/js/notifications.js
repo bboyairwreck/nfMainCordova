f481ddd3178f19cabe81b6e9b1df8482434b4b4b
Eric Chee <bboyairwreck@gmail.com>
2015-05-24 16:32:56 -0700

    Reminder Notifications are scheduled and show up at appropriate time

--- www/js/notifications.js
+++ www/js/notifications.js
@@ -13,9 +13,38 @@ function loadNotif() {
         success: receivedAllRemindersOfPatient,
         error: ajaxError
     });
+
+    // Show Notification Modal on receive
+    cordova.plugins.notification.local.on("trigger", function(notification) {
+        //alert("triggered: " + notification.id);
+        var dataJSON = JSON.parse(notification.data);
+
+        var curEventDate = new Date(dataJSON["eventTime"]);
+        var curEventID = dataJSON["eventID"];
+
+        var $notifModalWrap = $("#notificationModalWrap");
+        $("#notificationModalWrap h1").text(dataJSON["eventTitle"]);
+        $notifModalWrap.fadeIn(500);
+
+    });
+
+    // Show Notification Modal on receive
+    cordova.plugins.notification.local.on("schedule", function(notification) {
+        //alert("triggered: " + notification.id);
+        var dataJSON = JSON.parse(notification.data);
+
+        var curEventDate = new Date(dataJSON["eventTime"]);
+
+        //alert(curEventDate);
+    });
 }
 
 function receivedAllRemindersOfPatient(data) {
+
+    // Clear All notifications
+    cordova.plugins.notification.local.clearAll(function() {
+        //alert("cleared All notifications");
+    }, this);
 
     // TODO need to add month's to value
     var timeUnitMap = {'Minutes' : 60*1000,
@@ -26,9 +55,10 @@ function receivedAllRemindersOfPatient(data) {
     for (var i = 0; i < data.length; i++) {
         var reminder = data[i];
 
-        // Get "EventID" and "EventTime"
+        // Get "EventID", "EventTitle" and "EventTime"
         var evID = reminder["EventID"];
         var eventTime = reminder["EventTime"];
+        var eventTitle = reminder["EventTitle"];
 
         // Get Reminder quantity
         var quantityNum = reminder["QuantityNum"];  // 5 or 10 units of time measure
@@ -38,40 +68,48 @@ function receivedAllRemindersOfPatient(data) {
         var remTypeMilli = timeUnitMap[remTypeName];        // i.e. 60*1000
 
         // get EventTime in milliseconds
-        var eventTimeMilli = dateTimeToDateObj(eventTime).getTime();   // get Datetime in milliseconds
+        var eventTimeObj = dateTimeToDateObj(eventTime);
+        var eventTimeMilli = eventTimeObj.getTime();   // get Datetime in milliseconds
 
         // get datetime of Reminder by subtracking EventTime - quant
         var reminderTime = new Date(eventTimeMilli - (quantityNum * remTypeMilli));
 
         var remID = reminder["ReminderID"];
 
-        //alert(dateTimeToDateObj(eventTime).toString() + "  vs  " + reminderTime.toString());
-
+        //alert(eventTimeObj.toString() + "  vs  " + reminderTime.toString());
+        scheduleNotification(remID, reminderTime, evID, eventTitle, eventTimeObj);
     }
 
 
-    var now = new Date().getTime();                 // TODO eventDateTime
-    var _5_sec_from_now = new Date(now + 2*1000);   // TODO
+    //var now = new Date().getTime();                 // TODO eventDateTime
+    //var _5_sec_from_now = new Date(now + 1*1000);   // TODO
+    //
+    //var evID = 1234;
+    //
+    //// Notifications
+    //cordova.plugins.notification.local.schedule({
+    //    id: 1,
+    //    at:_5_sec_from_now,
+    //    data: {
+    //        eventID   : evID,
+    //        eventTitle:"This is data from a notification",
+    //        eventTime : _5_sec_from_now
+    //    }
+    //});
 
+
+}
+
+function scheduleNotification(remID, remTime, evID, evTitle, evTime) {
     // Notifications
     cordova.plugins.notification.local.schedule({
-        id: 1,
-        at:_5_sec_from_now,
-        data: { eventName:"This is data from a notification"}
-    });
-
-    cordova.plugins.notification.local.on("trigger", function(notification) {
-        //alert("triggered: " + notification.id);
-        var dataJSON = JSON.parse(notification.data);
-
-        alert(dataJSON["eventName"]);
-
-        var $notifModalWrap = $("#notificationModalWrap");
-        $("#notificationModalWrap h1").text(dataJSON["eventName"]);
-        $notifModalWrap.fadeIn(1000);
-        //$notifModalWrap.css("display", "block");
-
-
+        id: remID,
+        at: remTime,
+        data: {
+            eventID   : evID,
+            eventTitle: evTitle,
+            eventTime : evTime
+        }
     });
 }
 
@@ -86,7 +124,6 @@ function dateTimeToDateObj(dateTimeString) {
 }
 
 $("#notifDismissBtn").on("touchend", function(){
-    alert("Dismiss button clicked");
     var $notifModalWrap = $("#notificationModalWrap");
     $notifModalWrap.fadeOut(300);
 });
