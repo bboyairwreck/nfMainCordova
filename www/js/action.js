740634f2449b6db0eaf0c52789ab31d302dd194e
Eric Chee <bboyairwreck@gmail.com>
2015-05-25 01:07:47 -0700

    Toggle Create and Call buttons with firebase

--- www/js/action.js
+++ www/js/action.js
@@ -18,8 +18,41 @@ $(document).ready(function() {
     if (callButton == 1) {   // call button only
         $callBtn.css("display", "block");
     }
+
+    // Firebase - Check for changes
+    var fire = new Firebase('https://neverforgotten.firebaseio.com/');
+    var patID = localStorage.getItem("patient");
+    var patFire = fire.child(patID);
+    patFire.on('child_changed', fbSettingChanged);
+    patFire.on('child_added', fbSettingChanged);
+
+    if (localStorage.getItem("greetingSound") == 1) {
+        speakPhrase("What would you like to do?");
+    }
 });
 
-if (localStorage.getItem("greetingSound") == 1) {
-    speakPhrase("What would you like to do?");
+
+function fbSettingChanged(snapshot) {
+    var value = snapshot.val();
+    var key = snapshot.key();
+    //alert("key = " + key + "; value = " + value);
+
+    if (key == "CreateReminderButton") {
+        var $createBtn = $("#aCreateBtn");
+
+        if (value == 0) {
+            $createBtn.css("display", "none");
+        } else {
+            $createBtn.css("display", "block");
+        }
+
+    } else if (key == "CallButton") {
+        var $callBtn = $("#aCallBtn");
+        if (value == 0) {
+            $callBtn.css("display", "none");
+        } else {
+            $callBtn.css("display", "block");
+        }
+    }
 }
+
